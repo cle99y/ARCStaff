a63a57b877e3c104193c8fc408d92e1158d86384
PaulClegg <chilemerlot@gmail.com>
2018-04-20 11:17:19 +0100

    180420ii

--- app/src/main/java/com/geeklife/arcstaff/CreateEmployee.java
+++ app/src/main/java/com/geeklife/arcstaff/CreateEmployee.java
@@ -54,7 +54,7 @@ public class CreateEmployee extends AppCompatActivity {
 
                 switch (v.getId()) {
                     case R.id.bus_unit:
-                        Toast.makeText( CreateEmployee.this, "clicked BU", Toast.LENGTH_SHORT ).show();
+                        Toast.makeText( CreateEmployee.this, "clicked BUS UNIT", Toast.LENGTH_SHORT ).show();
                         break;
                     case R.id.home_office:
                         Toast.makeText( CreateEmployee.this, "clicked Office", Toast.LENGTH_SHORT ).show();
