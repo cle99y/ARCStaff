41e203839d899c86cc4323bd681fbe0a03865405
cle99y <chilemerlot@gmail.com>
2018-04-22 20:46:14 +0100

    180422

--- app/src/main/java/com/geeklife/arcstaff/CreateEmployee.java
+++ app/src/main/java/com/geeklife/arcstaff/CreateEmployee.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,15 +24,18 @@ import java.net.URL;
 
 public class CreateEmployee extends AppCompatActivity {
 
+    final String OFFICE = "office";
+    final String BUSINESS = "business";
+
     EditText fName, lName, bUnit, hOffice, emailAdd, contact;
 
     ListView buList, offList;
     // required lists
     String buItems[] = {"Defence", "Nuclear", "Oil & Gas", "Operations", "Rail", "Resourcing"};
-    String bu[] = {"DBU", "NBU", "OBU", "OPS", "RBU"};
-    String countryList[] = {"United Kingdom (+44)", "Australia (+61)", "Singapore (+51)"};
-    String officeList[] = {"Edinburgh", "Glasgow", "Manchester", "Bristol", "Taunton", "london",
-            "Singapore", "Pert", "Sydney"};
+    String buNames[] = {"DBU", "NBU", "OBU", "OPS", "RBU", "RMG"};
+    String countryItems[] = {"United Kingdom (+44)", "Australia (+61)", "Singapore (+51)"};
+    String officeItems[] = {"Edinburgh", "Glasgow", "Manchester", "Bristol", "Taunton", "London",
+            "Singapore", "Perth", "Sydney"};
 
     JSONObject employee;
 
@@ -40,7 +44,7 @@ public class CreateEmployee extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.new_user );
 
-        final InputMethodManager imm = (InputMethodManager ) getSystemService( INPUT_METHOD_SERVICE );
+        final InputMethodManager imm = ( InputMethodManager ) getSystemService( INPUT_METHOD_SERVICE );
 
         Button enter = findViewById( R.id.btn_enter );
 
@@ -51,6 +55,7 @@ public class CreateEmployee extends AppCompatActivity {
         emailAdd = findViewById( R.id.email );
         contact = findViewById( R.id.contact );
         buList = findViewById( R.id.list_view );
+        offList = findViewById( R.id.list_view );
 
         employee = new JSONObject();
 
@@ -63,20 +68,26 @@ public class CreateEmployee extends AppCompatActivity {
                     // focus changes to BU
                     case R.id.bus_unit:
                         if ( hasFocus ) {
-                            imm.hideSoftInputFromWindow(bUnit.getWindowToken(),0);
+                            Log.i("FORM", "BU has focus");
+                            imm.hideSoftInputFromWindow( bUnit.getWindowToken(), 0 );
                             setArrayAdapter( buItems );
                             toggleVisibility();
                         }
                         break;
                     // focus changes to HO
                     case R.id.home_office:
-                        if ( hasFocus )
-                            hOffice.setText( "MAN" );
+                        if ( hasFocus ) {
+                            Log.i("FORM", "OFFICE has focus");
+                            imm.hideSoftInputFromWindow( hOffice.getWindowToken(), 0 );
+                            setArrayAdapter( officeItems );
+                            toggleVisibility();
+                        }
                         break;
                 }
 
             }
         };
+
 
         enter.setOnClickListener( new View.OnClickListener() {
             @Override
@@ -98,6 +109,22 @@ public class CreateEmployee extends AppCompatActivity {
                 Log.i( "BTN", employee.toString() );
                 new AsyncCreateEmp().execute();
 
+            }
+        } );
+
+        buList.setOnItemClickListener( new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick( AdapterView<?> adapterView, View view, int pos, long len ) {
+                bUnit.setText( buNames[pos] );
+                toggleVisibility();
+            }
+        } );
+
+        offList.setOnItemClickListener( new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick( AdapterView<?> adapterView, View view, int pos, long len ) {
+                hOffice.setText( officeItems[pos] );
+                toggleVisibility();
             }
         } );
 
@@ -134,6 +161,7 @@ public class CreateEmployee extends AppCompatActivity {
     }
 
     private ArrayAdapter setArrayAdapter( String arrayToUse[] ) {
+
         ArrayAdapter mAdapter = new ArrayAdapter<>
                 ( this, R.layout.lv_rows, arrayToUse );
         buList.setAdapter( mAdapter );
@@ -143,12 +171,12 @@ public class CreateEmployee extends AppCompatActivity {
 
     private void toggleVisibility() {
         ConstraintLayout form = findViewById( R.id.new_user_form );
-        ListView listView = findViewById( R.id.list_view );
+        Log.i( "FORM", Integer.toString( form.getChildCount() ) );
 
         for ( int i = 0; i < form.getChildCount(); i++ ) {
             View v = form.getChildAt( i );
-            Log.i( "LAYOUT", v.getResources().getResourceName( v.getId() ) );
-            if (v.getVisibility() == View.GONE) {
+
+            if ( v.getVisibility() == View.GONE ) {
                 v.setVisibility( View.VISIBLE );
             } else {
                 v.setVisibility( View.GONE );
